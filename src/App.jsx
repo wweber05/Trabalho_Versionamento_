efdbcb639a4d877d1607e2f766178434b827567e
wweber05 <willianweber123@gmail.com>
2026-04-10 15:13:35 -0300

    Adição de função onClick para console

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+    console.log(`O contador subiu para: ${count + 1}`);
+  };
+
   return (
     <>
       <section id="center">
+        <div>
+        </div>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1 id="title">Trabalho Bimestral Devops</h1>
@@ -23,8 +26,10 @@ function App() {
         <div className="card">
           <button
             className="counter"
-            onClick={() => setCount((count) => count + 1)}
-          >
+            onClick={handleIncrement}
+            style={{ color: count > 10 ? 'gold' : 'white' }}
+            >
+              
             Contador está em {count}
           </button>
 
@@ -35,8 +40,7 @@ function App() {
           {count > 10 && <p>🔥 Contador passou de 10!</p>}
         </div>
       </section>
-      
-      <p>Data: {new Date().toLocaleDateString()}</p>
+
       <footer>
         <p>Patrick tem grande futuro como professor</p>
       </footer>
